6c7cbc16bad9aefbfc2f5f29e85013965750fa8a
ArjunSuresh0011 <arjunsuresh0211@gmail.com>
2022-07-12 22:17:03 +0530

    relational operators

---
+++ relational.js
@@ -0,0 +1,23 @@
+// relational operators or comparison operators
+
+let a = 10
+let b = 20
+
+console.log(a > b);
+console.log(b > a);
+
+let c = '3'
+let d = 5
+
+console.log(c < d);    //the string is converted to number
+
+let e = 'Arjun'
+let f = "Arjun s"
+
+console.log(e < f);  //the string comparison based on thire asci values
+
+let g = 20
+let h = 20
+
+console.log(g === h); // comparing the type and values
+console.log(g !== h);
