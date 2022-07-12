2c2d3518a25ae2d38c9b7fcebc8c37cf67d2584a
ArjunSuresh0011 <arjunsuresh0211@gmail.com>
2022-07-12 21:29:37 +0530

    Arithmetic operations

---
+++ arithmetic.js
@@ -0,0 +1,30 @@
+
+// arithmetic operators 
+
+let number1 = 10
+let number2 = 20
+
+let result = number1+number2
+let sub = number1-number2
+let mul = number1*number2
+let div = number1 / number2
+let reminder = number1 % number2
+let exponent = number1 ** number2
+
+let increment = number1++
+let decrement = number1--
+let preIncrement = ++number1
+let preDecrement = --number1
+
+
+console.log('sum is = '+ result)
+console.log('difference = '+sub)
+console.log('product = '+mul)
+console.log('division = '+div)
+console.log("reminder = " + reminder)
+console.log("exponent = " + exponent)
+console.log("increment = " + increment);
+console.log("decrement = " + decrement);
+console.log("pre increment = " + preIncrement);
+console.log("pre decrement = " + preDecrement);
+
