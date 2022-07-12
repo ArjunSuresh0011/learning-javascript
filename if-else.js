2b2dd3697d966afac8fb64247cc5db30aaad03e6
ArjunSuresh0011 <arjunsuresh0211@gmail.com>
2022-07-12 23:10:03 +0530

    if else statement

---
+++ if-else.js
@@ -0,0 +1,30 @@
+// conditional statement
+
+let a = 10;
+let b = 200;
+let c = 30;
+
+if (a > b && a>c){
+   console.log("a is gratest"); 
+}
+else if(b>a && b>c){
+    console.log("b is gratest");
+}
+else{
+    console.log("c is gratest");
+}
+
+
+// check if given number is even or odd
+
+let num = 329651 * 3;
+
+if (num%2===0){
+    console.log("The number is even");
+}
+else if (num%2 === 0){
+    console.log("The number is odd")
+}
+else {
+    console.log("This is not a number");
+}
