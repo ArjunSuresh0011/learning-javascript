8a6d1aab4576bb75cb896c101c81e8088b7b0c9e
ArjunSuresh0011 <arjunsuresh0211@gmail.com>
2022-07-12 19:13:10 +0530

    type convertion

--- helloworld.js
+++ helloworld.js
@@ -3,3 +3,43 @@ let num = 10
 let num2=20
 let sum=num+num2
 console.log(sum)
+console.log(typeof num)
+
+console.log(Number('30777'))
+console.log(String("hello"))
+console.log(parseInt('234 hello'))
+console.log(parseFloat('255.5 helloworld'))
+
+// unari +
+
+let x='55'
+let y=+x
+console.log(y)
+
+//converting number to string
+
+a = (123+555)
+a.toString()
+console.log(a)
+
+c= new Date()
+console.log(Number(c))
+console.log(c.getTime())
+
+console.log(String(Date()))
+
+let get = new Date()
+
+console.log(get.getDate())
+console.log(get.getMonth())
+
+//bool to number
+
+console.log(Number(true));
+console.log(Number(Number(false)));
+
+// bool to string
+
+console.log(String(true))
+console.log(String(false))
+console.log(true.toString())
