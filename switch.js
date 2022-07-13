72ee7887f16747a791cf2490ea81748bee58aed7
ArjunSuresh0011 <arjunsuresh0211@gmail.com>
2022-07-13 21:53:06 +0530

    switch case and template literal

---
+++ switch.js
@@ -0,0 +1,20 @@
+// if the number is one print 1
+
+let num = 'four' ;
+
+switch(num){
+    case 'one' : 
+    console.log('1');
+    break;
+
+    case 'two' :
+    console.log('2');
+    break;
+
+    case 'three' :
+    console.log('3');
+    break;
+
+    default : 
+    console.log('out of range(only 1-3)');
+}
