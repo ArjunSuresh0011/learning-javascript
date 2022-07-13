846792a498a22ab608ee1c618d21e738db9a072d
ArjunSuresh0011 <arjunsuresh0211@gmail.com>
2022-07-13 21:56:11 +0530

    switch case and template literal

--- switch.js
+++ switch.js
@@ -18,3 +18,15 @@ switch(num){
     default : 
     console.log('out of range(only 1-3)');
 }
+
+
+// Template litteral
+
+let num1 = 10
+let num2 = 20
+let sum = num1 + num2
+
+console.log(`The sum of ${num1} and ${num2} is ${sum}`);
+
+
+
