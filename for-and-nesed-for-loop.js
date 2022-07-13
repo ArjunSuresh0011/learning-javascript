9830f605423e02df2c399ffb6c2ecb404e496289
ArjunSuresh0011 <arjunsuresh0211@gmail.com>
2022-07-13 23:03:09 +0530

    for and nested for loop

---
+++ for-and-nesed-for-loop.js
@@ -0,0 +1,14 @@
+// printing first 10 numbers
+
+for(i=1;i<10;i++){
+    console.log(i);
+}
+
+// nested for loop
+
+for(i=0;i<=10;i++){
+    console.log('Arju')
+    for(j=0;j<i;j++){
+        console.log('suresh')
+    }
+}
