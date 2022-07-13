8d786ffbbfae22c3945e4533568e1e4035bb87ea
ArjunSuresh0011 <arjunsuresh0211@gmail.com>
2022-07-13 21:14:51 +0530

    if else , Ternery operator

--- if-else.js
+++ if-else.js
@@ -4,35 +4,32 @@ let a = 10;
 let b = 200;
 let c = 30;
 
-if (a > b && a>c){
-   console.log("a is gratest"); 
+if (a > b && a > c) {
+  console.log("a is gratest");
+} else if (b > a && b > c) {
+  console.log("b is gratest");
+} else {
+  console.log("c is gratest");
 }
-else if(b>a && b>c){
-    console.log("b is gratest");
-}
-else{
-    console.log("c is gratest");
-}
-
 
 // check if given number is even or odd
 
 let num = 329651 * 3;
 
-if (num%2===0){
-    console.log("The number is even");
-}
-else if (num%2 === 0){
-    console.log("The number is odd")
-}
-else {
-    console.log("This is not a number");
+if (num % 2 === 0) {
+  console.log("The number is even");
+} else if (num % 2 === 0) {
+  console.log("The number is odd");
+} else {
+  console.log("This is not a number");
 }
 
-// we can also check even or odd using ternery operator 
+// we can also check even or odd using ternery operator
 
-let number = 21
+let number = 21;
 
-let even_odd = number % 2 ===0 ? "The given number is Even" : "The given number is odd" 
+let even_odd =
+  number % 2 === 0 ? "The given number is Even" : "The given number is odd";
 
 console.log(even_odd);
+
