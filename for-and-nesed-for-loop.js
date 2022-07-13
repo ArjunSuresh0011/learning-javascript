e08b9891cd4fa8b437318c21647667f8a5b6f8a4
ArjunSuresh0011 <arjunsuresh0211@gmail.com>
2022-07-13 23:17:33 +0530

    for and nested for loop

--- for-and-nesed-for-loop.js
+++ for-and-nesed-for-loop.js
@@ -6,9 +6,17 @@ for(i=1;i<10;i++){
 
 // nested for loop
 
-for(i=0;i<=10;i++){
-    console.log('Arju')
+for(i=0;i<=1;i++){
+    console.log('Arjun')
     for(j=0;j<i;j++){
         console.log('suresh')
+    }
+}
+
+// values divisible by 3 between 1-100
+
+for(let i=1;i<100;i++){
+    if(i%3===0){
+        console.log(i);
     }
 }
