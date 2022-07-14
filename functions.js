3278a12a2d4a0767e8a27f08d4b0fcb26cd8c44d
ArjunSuresh0011 <arjunsuresh0211@gmail.com>
2022-07-14 20:39:20 +0530

    functions and function expression

---
+++ functions.js
@@ -0,0 +1,22 @@
+//  functions in js
+
+function add(a,b){
+    return sum
+}
+
+let num1 = 10;
+let num2 = 20;
+let sum = num1+num2;
+
+console.log(`sum of ${num1} and ${num2} is ${add(sum)}`+'\n');
+
+
+// function Expression
+
+let mul = function(a,b){
+    return product
+}
+
+product = num1*num2
+
+console.log(`product of ${num1} and ${num2} is ${mul(product)}`);
