1d5a86345a490392a4c53611af524df3a1a285ee
ArjunSuresh0011 <arjunsuresh0211@gmail.com>
2022-07-14 22:06:09 +0530

    Arrow function

---
+++ arrow-function.js
@@ -0,0 +1,18 @@
+// arrow function dont need function keyword
+
+let negetive = (num1,num2) => {
+    return sum
+}
+
+a = 9920
+b= 50
+sum = a+b
+
+if (a > 0 && b > 0) {
+  console.log(negetive(sum));
+} else {
+  console.log("dont allow negative numbers");
+}
+
+
+
