fceaccf230dfdc6e450f8b4238945f4beff48f40
ArjunSuresh0011 <arjunsuresh0211@gmail.com>
2022-07-15 12:38:23 +0530

    constructor function with use of this keywors

---
+++ constructor-function.js
@@ -0,0 +1,20 @@
+// constructor function and use of this keyword
+
+
+function Employee(name,salary,place){
+    this.name = name
+    this.salary = salary
+    this.palce = place
+    this.position = 'sales assistent'
+
+    this.pos = function(){
+        console.log(`${this.name} is from ${this.palce} have position of ${this.position}`)
+    } 
+}
+
+
+
+obj1 = new Employee('Arjun',20000,'kollam')
+
+console.log(obj1);
+console.log(obj1.pos());
