d54a3e1c602176c1dc685a1e7a66c71a49b5fefc
ArjunSuresh0011 <arjunsuresh0211@gmail.com>
2022-07-15 13:41:30 +0530

    Array creation and multiple data type in an array and fetch those values

--- array.js
+++ array.js
@@ -16,5 +16,18 @@ console.log(data)
 // now fetching data from an array
 
 console.log(`third data is ${data[2]}`);
+console.log(`+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n`);
 
+// now we are creating mixed type arrays
 
+let details = [
+    'Arjun',
+    20000,
+    {tech : 'js'},
+    function(){console.log("hello world")}
+]
+
+console.log(`name :   ${details[0]}`);
+console.log(`salary :   ${details[1]}`);
+console.log(`technology :   ${details[2].tech}`);
+console.log(`message :   ${details[3]()}`);
