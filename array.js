07a51c3660fd0d291b6c6fd6350fe5552867eab4
ArjunSuresh0011 <arjunsuresh0211@gmail.com>
2022-07-15 13:27:02 +0530

    Array creation and fetching data

---
+++ array.js
@@ -0,0 +1,20 @@
+// creating an empty array
+
+let data  = [] ;  //or
+
+let values = new Array() ;
+
+// now add data to array and print array
+
+data[0] = 10 ;
+data[1] = 20 ;
+data[2] = 30 ;
+
+console.log(data)
+
+
+// now fetching data from an array
+
+console.log(`third data is ${data[2]}`);
+
+
