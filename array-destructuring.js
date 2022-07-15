b99b237dbefcb38911262ed949a8dc81aa78d6c2
ArjunSuresh0011 <arjunsuresh0211@gmail.com>
2022-07-15 18:21:09 +0530

    Array destructuring

---
+++ array-destructuring.js
@@ -0,0 +1,15 @@
+let arr = [2,3,4,5,6,7]
+
+let [a,...d] =arr
+
+console.log(a);
+console.log(d);
+
+let words = 'my name is arjun suresh'.split(' ')
+
+console.log(words);
+let [g,t,h,m,f] =words
+
+for(let n of words){
+    console.log(n)
+}
